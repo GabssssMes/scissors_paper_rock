4b864523a87be5a22aef58a20e8f5a14598d6ff4
Rubatore <messner92@gmail.com>
2023-03-24 19:34:39 +0100

    -Trivial game was made -You can see the results on the console

---
+++ javascript.js
@@ -0,0 +1,42 @@
+function getComputerchoice(){
+    let x=Math.floor(Math.random()*3);
+
+
+    if(x===0)return x="scissors";
+    else if(x===1)return x="paper";
+    else return x="rock";
+}
+function getPlayerschoice(){
+    let x=prompt("Whats your choise? Scissors, paper or rock?");
+    //console.log(x.toLowerCase);
+    return x.toLowerCase();
+}
+function playRound(player,computer){
+    if(player===computer) return "draw";
+    switch(player){
+        case("rock"):switch(computer){
+            case("scissors"):return "player";
+            case("paper"):return "computer";
+        }
+        case("scissors"): switch(computer){
+            case("rock"): return "computer";
+            case("paper"): return "player";
+        }
+        case("paper"): switch(computer){
+            case("rock"): return "player";
+            case("scissors"): return "computer";
+        }
+    }
+}
+let firstto=3;
+let erg;
+let computer=0, player=0;
+while(computer<3 && player<3){
+    erg=playRound(getPlayerschoice(),getComputerchoice());
+    console.log(erg);
+    if(erg==="draw") continue;
+    else if(erg==="player") player++;
+    else if(erg==="computer") computer++;
+}
+if(player===3) console.log("Player wins");
+else console.log("Computer wins");
