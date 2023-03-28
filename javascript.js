930dedce61161ec597369caf6cd3f9560e0dd2a2
Rubatore <messner92@gmail.com>
2023-03-28 08:54:29 +0200

    ui for spr game was created

--- javascript.js
+++ javascript.js
@@ -1,16 +1,13 @@
 function getComputerchoice(){
     let x=Math.floor(Math.random()*3);
+    com=document.querySelector(".computer");
 
 
-    if(x===0)return x="scissors";
-    else if(x===1)return x="paper";
-    else return x="rock";
+    if(x===0){com.textContent="✌️";return x="scissors";}
+    else if(x===1){com.textContent="✋";return x="paper";}
+    else {com.textContent="✊";return x="rock";}
 }
-function getPlayerschoice(){
-    let x=prompt("Whats your choise? Scissors, paper or rock?");
-    //console.log(x.toLowerCase);
-    return x.toLowerCase();
-}
+
 function playRound(player,computer){
     if(player===computer) return "draw";
     switch(player){
@@ -28,18 +25,42 @@ function playRound(player,computer){
         }
     }
 }
-function game(firstto){
-let erg;
-let computer=0, player=0;
-while(computer<firstto && player<firstto){
-    erg=playRound(getPlayerschoice(),getComputerchoice());
-    console.log(erg);
-    if(erg==="draw") continue;
-    else if(erg==="player") player++;
-    else if(erg==="computer") computer++;
-}
-if(player===3) console.log("Player wins");
-else console.log("Computer wins");
+function incrementcomputer(computer){
+    const com=document.getElementById("2");
+    com.textContent=computer;
+    return;
 }
 
-game(3);
+function incrementplayer(player){
+    const com=document.getElementById("1");
+    com.textContent=player;
+    return;
+}
+
+function game(firstto){
+    let erg;
+    let computer=0, player=0;
+    const buttons=document.querySelectorAll('button');
+    const ergebnis=document.querySelector(".choice");
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            console.log(button.id);
+            erg=playRound(button.id,getComputerchoice());
+            console.log(erg);
+            if(erg==="player"){
+                player++;
+                incrementplayer(player);
+            }
+            else if(erg==="computer"){
+                computer++;
+                incrementcomputer(computer);
+            }
+            if(player===firstto) ergebnis.textContent="You won!\nReload for a new Game!";
+            else if(computer===firstto) ergebnis.textContent="You lost!\nReload for a new Game!";
+            
+        });
+    });
+}  
+
+game(5);
+
